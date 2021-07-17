1c14a04439b9a1145f44ce7978e725ba69392a47
Bart <bart@bamweb.nl>
2021-07-17 09:34:12 +0200

    log extract and especially get the top 'x' results

---
+++ log_extracht_top10.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import re
+
+FILE="files/apache_logs.txt"
+count_dict = {}
+
+with open(FILE, 'r') as f:
+    content = f.readlines()
+
+slash16 = re.compile('\d+\.\d+')
+ips = [x.split()[0] for x in content]
+subnets = [slash16.match(x).group(0) for x in ips]
+
+for subnet in subnets:
+    if count_dict.get(subnet):
+        count_dict[subnet] += 1
+    else:
+        count_dict[subnet] = 1
+
+top10_sorted = sorted(count_dict.items(), key=lambda item: item[1], reverse=True)[:10]
+
+for subnet, count in top10_sorted:
+    print(f'{count} - {subnet}')
